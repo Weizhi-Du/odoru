6f39159d49ce6ab74098fd8607556257623ed632
retepViolet <fudanpeter@hotmail.com>
2024-10-19 23:30:04 -0500

    match_class

--- match/match.py
+++ match/match.py
@@ -1,14 +1,64 @@
 import cv2
 import mediapipe as mp
-from tqdm import tqdm
 import numpy as np
 from scipy.spatial import procrustes
 
 
-# 模型配置
-mp_pose = mp.solutions.pose
-pose_landmark = mp_pose.PoseLandmark
-mp_drawing = mp.solutions.drawing_utils
+class PoseMatcher:
+    def __init__(self, w = 270, h = 480, max_step = 10):
+        self.w, self.h = w, h
+        self.max_step = max_step
+        self.batch_action, self.batch_target = [], []
+        self.score = 0
+        # 模型配置
+        self.mp_pose = mp.solutions.pose
+        self.pose_landmark = self.mp_pose.PoseLandmark
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.model1, self.model2 =  self.mp_pose.Pose(), self.mp_pose.Pose()
+
+    # 动作识别
+    def detect_pose(self, model, frame):
+        res = model.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        return res
+
+    # 单帧匹配
+    def match(self, action, target):
+        if action.pose_landmarks is None or target.pose_landmarks is None: return 100
+        action = action.pose_landmarks.landmark
+        target = target.pose_landmarks.landmark
+        body1, body2 = [], []
+        for i in [0, 11, 12, 13, 14, 21, 22, 23, 24, 25, 26, 27, 28]:
+            if action[i].visibility < 0.5 or target[i].visibility < 0.5: continue
+            body1.append([action[i].x * self.w , action[i].y * self.h])
+            body2.append([target[i].x * self.w, target[i].y * self.h])
+        _, _, disparity = procrustes(np.array(body1), np.array(body2))
+        return disparity * 1e2
+
+    # 批量匹配
+    def match_batch(self, action, target):
+        best = 1e9
+        for i in action:
+            for j in target:
+                best = min(best, self.match(i, j))
+        return best
+
+    # 绘制骨骼
+    def draw(self, frame, result):
+        if result.pose_landmarks:
+            self.mp_drawing.draw_landmarks(frame, result.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+                
+    # 调用
+    def __call__(self, frame1, frame2):
+        res1, res2 = self.detect_pose(self.model1, frame1), self.detect_pose(self.model2, frame2)
+        self.batch_action.append(res1); self.batch_target.append(res2)
+        if len(self.batch_target) == self.max_step:
+            self.score = self.match_batch(self.batch_action, self.batch_target)
+            self.batch_action, self.batch_target = [], []
+        self.draw(frame1, res1); self.draw(frame2, res2)
+        return frame1, frame2, self.score
+
+
+##########################
 
 
 # 读取视频
@@ -26,62 +76,18 @@ def read_video(path):
     return frames
 
 
-# 动作识别
-def detect_pose(model, frame):
-    res = model.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    return res
-
-
-# 动作匹配 （一帧）
-def match(action, target, w, h):
-    if action.pose_landmarks is None or target.pose_landmarks is None: return 100
-    action = action.pose_landmarks.landmark
-    target = target.pose_landmarks.landmark
-    # action 原点
-    body1, body2 = [], []
-    for i in [0, 11, 12, 13, 14, 21, 22, 23, 24, 25, 26, 27, 28]:
-        if action[i].visibility < 0.5 or target[i].visibility < 0.5: continue
-        body1.append([action[i].x * w , action[i].y * h])
-        body2.append([target[i].x * w, target[i].y * h])
-    _, _, disparity = procrustes(np.array(body1), np.array(body2))
-    return disparity * 1e2
-
-
-# 批量匹配
-def match_batch(action, target, w, h):
-    best = 1e9
-    for i in action:
-        for j in target:
-            best = min(best, match(i, j, w, h))
-    return best
-
-
-def draw(frame, result):
-    if result.pose_landmarks:
-        mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-
 # main
 def compare():
+    w, h = 270, 480
+    model = PoseMatcher(w, h)
     action = read_video('./match/weizhi.mp4')
     target = read_video('./match/weien.mp4')[30:]
-    w, h = 270, 480
-    batch_action, batch_target = [], []
-    score = 1
-    model1, model2 = mp_pose.Pose(), mp_pose.Pose()
     for i in range(max(len(action), len(target))):
         frame1, frame2 = cv2.resize(action[-1], (w, h)), cv2.resize(target[-1], (w, h))
         if i < len(action): frame1 = cv2.resize(action[i], (w, h))
         if i < len(target): frame2 = cv2.resize(target[i], (w, h))
-
-        res1, res2 = detect_pose(model1, frame1), detect_pose(model2, frame2)
-        batch_action.append(res1)
-        batch_target.append(res2)
-        if len(batch_target) == 5:
-            score = match_batch(batch_action, batch_target, w, h)
-            batch_action, batch_target = [], []
-
-        draw(frame1, res1); draw(frame2, res2)
+        
+        frame1, frame2, score = model(frame1, frame2)
         frame = cv2.hconcat([frame1, frame2])
         cv2.putText(frame, str(round(score,1)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3)
         cv2.imshow("video", frame)
@@ -89,6 +95,7 @@ def compare():
 
 
 if __name__ == '__main__':
+    # compare()
     target = read_video('./match/weien.mp4')
     cap = cv2.VideoCapture(0)
     w, h = 270, 480
@@ -97,22 +104,15 @@ if __name__ == '__main__':
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(output_file, fourcc, 30, (w*2, h))
 
-    score, i = 0, 0
-    batch_action, batch_target = [], []
-    model1, model2 = mp_pose.Pose(), mp_pose.Pose()
+    i = 0
+    model = PoseMatcher(w, h)
     while cap.isOpened():
         ret, frame1 = cap.read()
         if not ret: break
         frame1, frame2 = cv2.flip(frame1[-h:, -w:], 1), cv2.resize(target[-1], (w, h))
         if i < len(target): frame2 = cv2.resize(target[i], (w, h))
         
-        res1, res2 = detect_pose(model1, frame1), detect_pose(model2, frame2)
-        batch_action.append(res1); batch_target.append(res2)
-        if len(batch_target) == 10:
-            score = match_batch(batch_action, batch_target, w, h)
-            batch_action, batch_target = [], []
-
-        draw(frame1, res1); draw(frame2, res2)
+        frame1, frame2, score = model(frame1, frame2)
         frame = cv2.hconcat([frame1, frame2])
         cv2.putText(frame, str(round(score,1)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3)
         out.write(frame)
